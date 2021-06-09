369fd1c6617c6ec4906ba14599d3572d07ab27d0
David V. Lu <davidvlu@gmail.com>
2021-06-09 15:16:56 -0400

    ROS 2 package with code directly from tf/transformations.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+package_name = 'tf_transformations'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='David V. Lu!!',
+    maintainer_email='davidvlu@gmail.com',
+    description='Reimplementation of the tf/transformations.py library',
+    license='BSD',
+    tests_require=['pytest'],
+)
